a7e1a73c894a39a85be17569ab2677408a1046df
joopeti <pyry.joona@helsinki.fi>
2015-10-16 21:18:20 +0300

    poistettiin viestikentät ja lisättiin ääniä

--- index.js
+++ index.js
@@ -3,26 +3,27 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = 0;
 app.use(express.static('sounds'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/sounds/*', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-  console.log('haluttiin ääntä');
-});
-
 io.on('connection', function(socket){
-  console.log('connected:');
-  socket.on('viesti', function(msg){
-    io.emit('viesti', msg);
-    console.log('message: ' + msg);
-  });
+  users++;
+
+  io.emit('soittajia', users);
+  console.log('user connected');
+
   socket.on('painallus', function(key){
     io.emit('painallus', key);
-  })
+  });
+
+  socket.on('disconnect', function(){
+    users--;
+    console.log('user disconnected');
+  });
 });
 http.listen(3000, function(){
   console.log('kuunnellaan *:3000');
